fd9498a07fd22862759efa7bfcebb034b075a728
matteocorti <matteocorti@0c20eeee-d81f-4673-9323-c92ccfcb069e>
2007-04-12 09:30:48 +0000

    compiles on cygwin

--- regen.sh
+++ regen.sh
@@ -1,9 +1,9 @@
 echo "Updating configuration..."
 echo "Running aclocal"
-aclocal
+aclocal-1.9
 echo "Running autoconf"
 autoconf
 echo "Running autoheader"
 autoheader
 echo "Running automake"
-automake
+automake-1.9
